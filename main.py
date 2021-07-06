0f423e767f90833b67bdee477eaa989b0e46f45f
lucasuait <69118979+lucasuait@users.noreply.github.com>
2021-07-06 17:23:42 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 
 def handle_message(update, context):
@@ -18,14 +18,21 @@ def handle_message(update, context):
     )
 
 
+def handle_start(update, context):
+    update.message.reply_text(
+        text=('Hola! para usarme, solo tienes que enviarme un sticker y yo te voy a devolver '
+              'el <i>file_id</i> para que puedas usar ese sticker en tu bot.\nTambién voy a devolverte el '
+              'update completo <b>por las dudas</b>')
+    )
+
+
 if __name__ == '__main__':
-
     updater = Updater(token=os.environ['TOKEN'], use_context=True)
-
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.sticker, handle_message))
+    dp.add_handler(
+        CommandHandler('start', handle_start)
+    )
     updater.start_polling()
-
     print('Bot esta más vivo que vivin')
-
     updater.idle()
